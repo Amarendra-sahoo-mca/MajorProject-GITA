b36d0511e1ea4a35f0c3e8ad71ac57d5bca29ac5
Amarendra-sahoo-mca <amarendrasahoo521@gmail.com>
2024-04-02 00:11:10 +0530

    slide bar work

--- indexpage.js
+++ indexpage.js
@@ -3,6 +3,11 @@ let sdiv=document.querySelector(".search");
 let sinp=document.querySelector(".input");
 let menu=document.querySelector(".xx");
 let options=document.querySelectorAll(".option");
+let img=document.querySelector(".image");
+let text=document.querySelector(".text");
+
+let n=0;
+let f=1;
 let flag=0;
 let clicked=0;
 sdiv.addEventListener("click",()=>{
@@ -18,13 +23,25 @@ console.log("div click");
 });
 
 sbtn.addEventListener("click",(e)=>{
+    
      if(flag==1){
         console.log("btn click");
         e.stopPropagation();
      }
+     else if(flag==0){
+        console.log("innitial search button click");
+        sbtn.disabled=true;
+     }
+     
 });
 sinp.addEventListener("click",(e)=>{
     e.stopPropagation();
+});
+sinp.addEventListener("keyup",(e)=>{
+let str=e.currentTarget.value;
+if(str != ""){
+    sbtn.disabled=false;
+}
 });
 menu.addEventListener("click",()=>{
     if(clicked==0){
@@ -41,3 +58,21 @@ menu.addEventListener("click",()=>{
     }
   
 });
+setInterval(()=>{
+
+    text.style.transform = `translateX(-${n*25}%)`;
+    img.style.transform = `translateX(-${n*25}%)`;
+    if(n==3){
+        f=0;
+    }
+    else if(n==0){
+        f=1;
+    }
+    if(f==1){
+        n++;
+    }
+    else{
+        n--;
+    }
+   
+}, 4000);
